585aaca7a28acaab3dd117fb587cfdd3deeff7f7
mulwhiski <marshalldennis27@gmail.com>
2019-03-01 15:35:19 +0300

    add index.html

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function player(name, turns, score) {
+  this.playerName=name;
+  this.turnStatus=turns;
+  this.totalScore=score;
+}
+
+$(document).ready(function() {
+  $("button#roll").click(function(event) {
+    event.preventDefault()
+}
+$("roll").click(function() {
+  math.floor(math.random()*13)
+  if(math<2) {
+    alert(dice rolled 1)
+  });  
+});
