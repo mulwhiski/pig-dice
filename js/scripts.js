cab60420fdd4538629a141748bf0ab22a91a73a3
mulwhiski <marshalldennis27@gmail.com>
2019-03-01 16:43:20 +0300

    add index.html modifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
-function player(name, turns, score) {
-  this.playerName=name;
-  this.turnStatus=turns;
-  this.totalScore=score;
-}
+var player = {
+  gameNumber = function(number)
+  this.number = number;
+});
 
 $(document).ready(function() {
   $("button#roll").click(function(event) {
     event.preventDefault()
 }
 $("roll").click(function() {
-  math.floor(math.random()*13)
+  math.floor(math.random()*100) +1
   if(math<2) {
     alert(dice rolled 1)
-  });  
+  });
 });
+roll: function() {
+  var die = object.create(die);
+  var roll = die.roll();
+  if (roll === 1){
+    this.over = true;
+    this.points = 0;
+  } else {
+    this.points += roll;
+}
+return roll;
